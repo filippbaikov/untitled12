65055cfc7339df05d7e64bd6ffc04d2bd1bea421
filipp <zeliboba0@bk.ru>
2021-04-25 15:41:13 +0300

    fff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        String [] str={"хуй ","пизда ","вымя ","ублюдок ","тварь "};
+        System.out.println(str[3]+str[4]+str[0]);
+
     }
-}
+
+    }
