4f20d93ff4b6dfedd4e628491956b7658f58fb6a
filipp <zeliboba0@bk.ru>
2021-04-25 15:42:58 +0300

    ffff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String [] str={"хуй ","пизда ","вымя ","ублюдок ","тварь "};
         System.out.println(str[3]+str[4]+str[0]);
+        System.out.println("ggg");
 
     }
 
